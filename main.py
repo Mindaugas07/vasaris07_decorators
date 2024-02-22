335e6fced6f22ab2e94d8c5787113178c3b83899
Mindaugas Minelga <minelga.m@gmail.com>
2024-02-22 20:46:29 +0200

    modify main.py

--- main.py
+++ main.py
@@ -538,40 +538,161 @@ from typing import Callable
 # Create any function that could prbably fail.
 
 
-from typing import Callable, Union
-from random import randint
+# from typing import Callable, Union
+# from random import randint
+# import logging
+
+# logging.basicConfig(
+#     level=logging.DEBUG,
+#     filename="data.log",
+#     filemode="a",
+#     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+#     datefmt="%d/%m/%Y %H:%M:%S",
+# )
+
+
+# def repeat_n_times_or_till_success(repeat_times):
+#     def attempt_function(fn: Callable):
+#         def wrapper(*args, **kwargs):
+#             for i in range(repeat_times):
+#                 try:
+#                     result = fn(*args, **kwargs)
+#                     logging.info(
+#                         f"Function succeeded after {i+1} attempts. Result: {result}"
+#                     )
+#                     return result
+#                 except Exception:
+#                     pass
+
+#         return wrapper
+
+#     return attempt_function
+
+
+# @repeat_n_times_or_till_success(repeat_times=7)
+# def random_division(number_to_divide: Union[int, float]) -> Union[int, float]:
+#     return number_to_divide / randint(0, 4)
+
+
+# print(random_division(5))
+
+# Create a class with various mathematical operations (at least 5 of them), utilizing a consolidated decorator to enhance method functionalitie.
+# Decorator/s must check type of the values passed to class instance methods and at the same time calcualte execution time .
+
+
+# class Math:
+
+#     def __init__(self, num_one: int, num_two: int) -> None:
+#         self.num_one = num_one
+#         self.num_two = num_two
+
+#     def adding_two_numbers(self) -> int:
+#         return self.num_one + self.num_two
+
+#     def subtracting_two_numbers(self) -> int:
+#         return self.num_one - self.num_two
+
+#     def division_two_numbers(self) -> int:
+#         return self.num_one / self.num_two
+
+#     def multiplication_two_numbers(self) -> int:
+#         return self.num_one * self.num_two
+
+#     def power_two_numbers(self) -> int:
+#         return self.num_one**self.num_two
+
+
+# three_and_four = Math(3, 4)
+
+# print(three_and_four.power_two_numbers())
+
+
+# from typing import Callable
+# import time
+
+
+# def checker(fn: Callable):
+#     def wrapper(self, *args, **kwargs):
+#         start_time = time.time()
+#         if all(isinstance(i, (int, float)) for i in args):
+#             my_func = fn(self, *args, **kwargs)
+#         else:
+#             print("--- %s seconds ---" % (time.time() - start_time))
+#             raise TypeError("Entered values is not numbers")
+#         print("--- %s seconds ---" % (time.time() - start_time))
+#         return my_func
+
+#     return wrapper
+
+
+# class Calculator:
+#     @checker
+#     def addition(self, a: float, b: float) -> float:
+#         try:
+#             return a + b
+#         except Exception as err:
+#             print(err)
+
+#     @checker
+#     def substraction(self, a: float, b: float) -> float:
+#         return a - b
+
+#     @checker
+#     def multiplication(self, a: float, b: float) -> float:
+#         return a * b
+
+#     @checker
+#     def division(self, a: float, b: float) -> float:
+#         return a / b
+
+#     @checker
+#     def raise_power(self, a: float, b: float) -> float:
+#         try:
+#             return pow(a, b)
+#         except Exception as err:
+#             print(err)
+
+
+# calc = Calculator()
+
+# print(calc.raise_power(21, 51))
+
+# Create a decorator factory that would slow the execution speed of the decorated function (lets call it delay decorator.).
+# Log the execution times to the file.
+
 import logging
+import time
+from typing import Callable
 
 logging.basicConfig(
     level=logging.DEBUG,
-    filename="data.log",
+    filename="time_logger.log",
     filemode="a",
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     datefmt="%d/%m/%Y %H:%M:%S",
 )
 
 
-def repeat_n_times_or_till_success(repeat_times):
-    def attempt_function(fn: Callable):
+def delayer(delay_time):
+    def timer(fn: Callable):
         def wrapper(*args, **kwargs):
-            for i in range(repeat_times):
-                try:
-                    result = fn(*args, **kwargs)
-                    logging.info(
-                        f"Function succeeded after {i+1} attempts. Result: {result}"
-                    )
-                    return result
-                except Exception:
-                    pass
+            start_time = time.time()
+            time.sleep(delay_time)
+            my_func = fn(*args, **kwargs)
+            logging.info("Execution time: %s seconds" % (time.time() - start_time))
+            return my_func
 
         return wrapper
 
-    return attempt_function
+    return timer
 
 
-@repeat_n_times_or_till_success(repeat_times=7)
-def random_division(number_to_divide: Union[int, float]) -> Union[int, float]:
-    return number_to_divide / randint(0, 4)
+@delayer(1)
+def raise_power(a: int, b: int) -> int:
+    return pow(a, b)
 
 
-print(random_division(5))
+print(raise_power(5, 5))
+
+# Create a Database management class that would setup all connections and implement CRUD opearations (inside class/classses). There should be a decorator that would handle possible errors. 
+# Please check what error hanling class SQLAlchemy provides.
